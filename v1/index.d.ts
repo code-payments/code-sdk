ae3d63b3f175b3b2361b5c48efa1788111a9f06d
Zelimir Fedoran <zelimir.fedoran@gmail.com>
2024-02-29 13:41:06 -0500

    updated release

---
+++ v1/index.d.ts
@@ -0,0 +1,11 @@
+import * as internal from '@code-wallet/elements';
+declare const _default: {
+    elements: {
+        create: (type: import("@code-wallet/intents").ElementType, options: Omit<import("@code-wallet/intents").ElementOptions, "signers">) => {
+            button?: internal.CodeElement | undefined;
+            card?: internal.CodeElement | undefined;
+        };
+    };
+};
+export default _default;
+//# sourceMappingURL=index.d.ts.map
